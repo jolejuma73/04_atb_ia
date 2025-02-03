fb6452f42c0476484aca8ed7ae4a203040fca49f
jolejuma73 <2016juma@gmail.com>
2025-02-03 22:48:19 +0100

    03/02/25 22:50 horas

--- script.js
+++ script.js
@@ -759,13 +759,13 @@ document.addEventListener("DOMContentLoaded", function() {
                     <th><img src="imagenes/02_tipo.png" alt="Icono de presentación" class="icono-presentacion">Tipo de Antiinfecioso</th>
                     <td>${antibiotic.type || "N/A"}</td></tr>
                 <tr>
-                    <th><img src="imagenes/03_dosis2.png" alt="Icono de dosis" class="icono-dosis">Dosis</th>
+                    <th><img src="imagenes/03_dosis.png" alt="Icono de dosis" class="icono-dosis">Dosis</th>
                     <td>${antibiotic.dose || "N/A"}</td></tr>
                 <tr>
                     <th><img src="imagenes/04_preparacion.png" alt="Icono de preparación" class="icono-preparacion">Preparación</th>
                     <td>${antibiotic.preparation || "N/A"}</td></tr>
                 <tr>
-                    <th><img src="imagenes/05_aspecto2.png" alt="Icono de Aspecto" class="icono-aspecto">Aspecto</th>
+                    <th><img src="imagenes/05_aspecto.png" alt="Icono de Aspecto" class="icono-aspecto">Aspecto</th>
                     <td>${antibiotic.appearance || "N/A"}</td></tr>
                 <tr>
                     <th><img src="imagenes/06_tiempo.png" alt="Icono de Tiempo de administración" class="icono-tiempo">Tiempo de administración</th>
